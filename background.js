3fd764ddbdfd1a2518995a91c5edc81ecd282382
Scott Robertson <scottymeuk@gmail.com>
2013-06-13 13:57:39 +0100

    Format JS

--- background.js
+++ background.js
@@ -1,45 +1,37 @@
 chrome.omnibox.onInputEntered.addListener(
-  function(domain) {
+  function(domain) {        
+		ping(domain, function(result) {
+			var data = JSON.parse(result.response);
+			
+      if (data.ip === null || data.domain === null) {
+				
+        if (data.domain !== null) {
+					var title = data.domain;
+				} else if(data.ip !== null) {
+					var title = data.ip;
+				} else {
+					var title = 'This Website';
+				}
              
-  		ping(domain, function(result) {
-  			
-  			var data = JSON.parse(result.response);
-
-  			if (data.ip === null || data.domain === null) {
-
-
-  				if (data.domain !== null) {
-  					var title = data.domain;
-  				} else if(data.ip !== null) {
-  					var title = data.ip;
-  				} else {
-  					var title = 'This Website';
-  				}
-               
-                chrome.tabs.create({'url': "http://isup.me/" + domain}, function(tab) {
-                    return true;
-                });
-                                
-  			} else {
-
-  				var notification = webkitNotifications.createNotification(
+        chrome.tabs.create({'url': "http://isup.me/" + domain}, function(tab) {
+          return true;
+        });
+                              
+			} else {
+				var notification = webkitNotifications.createNotification(
 				  'img/Chrome.png',
 				  data.domain + ' is available',  
 				  data.ip 
-				);
-                
-                notification.show();
-  			}
-
-  		});
-
-  });
-
+				);   
+        notification.show();
+			}
+		});
+  }
+);
 
 function ping(domain, callback) {
-		
+  
 	var obj = new XMLHttpRequest();  
-
 	obj.onreadystatechange = function() {
 		if(obj.readyState == 4) {
 			callback(obj);
@@ -47,9 +39,7 @@ function ping(domain, callback) {
 	}
 
 	var apiKey = '6ad80f62e94579dd6c539176128f9cf2';
-
 	obj.open('get', 'http://scottrobertson.me/api/ping/' + domain + '?apikey=' + apiKey, true);
 	obj.send();
 	return obj; 
-
 }
