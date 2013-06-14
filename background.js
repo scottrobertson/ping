83d7d161c076753b579ff592e5f766e7bd8f68a6
Scott Robertson <scottymeuk@gmail.com>
2013-06-14 16:00:01 +0100

    Fix formatting

--- background.js
+++ background.js
@@ -1,32 +1,32 @@
 chrome.omnibox.onInputEntered.addListener(
-  function(domain) {        
-		ping(domain, function(result) {
-			var data = JSON.parse(result.response);
-			
-      if (data.ip === null || data.domain === null) {
-				
-        if (data.domain !== null) {
-					var title = data.domain;
-				} else if(data.ip !== null) {
-					var title = data.ip;
-				} else {
-					var title = 'This Website';
-				}
+    function(domain) {        
+    	ping(domain, function(result) {
+    		var data = JSON.parse(result.response);
+    		
+            if (data.ip === null || data.domain === null) {
+
+                if (data.domain !== null) {
+    				var title = data.domain;
+    			} else if(data.ip !== null) {
+    				var title = data.ip;
+    			} else {
+    				var title = 'This Website';
+    			}
              
-        chrome.tabs.create({'url': "http://isup.me/" + domain}, function(tab) {
-          return true;
-        });
-                              
-			} else {
-				var notification = webkitNotifications.createNotification(
-				  'img/Chrome.png',
-				  data.domain + ' is available',  
-				  data.ip 
-				);   
-        notification.show();
-			}
-		});
-  }
+                chrome.tabs.create({'url': "http://isup.me/" + domain}, function(tab) {
+                  return true;
+                });     
+                            
+    		} else {
+    			var notification = webkitNotifications.createNotification(
+    			    'img/Chrome.png',
+    			    data.domain + ' is available',  
+    			    data.ip 
+    			);   
+                notification.show();
+    		}
+    	});
+    }
 );
 
 function ping(domain, callback) {
